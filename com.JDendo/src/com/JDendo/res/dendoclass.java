1956ca5907739b7eccdfea533e3a0738ac038d17
Jose Ferdel Dorantes Lara <ferdeldo153@gmail.com>
2021-03-02 21:41:22 -0600

    Add files via upload

---
+++ com.JDendo/src/com/JDendo/res/dendoclass.java
@@ -0,0 +1,48 @@
+package com.JDendo.res;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author home
+ */
+public class dendoclass {
+    private String a="";
+    private String b="";
+    private double seme=0.0;
+    public dendoclass(String a1,String b1,double s){
+        this.a=a1;
+        this.b=b1;
+        this.seme=s;
+    }
+
+    /**
+     * @return the a
+     */
+    public String getA() {
+        return a;
+    }
+
+    /**
+     * @return the b
+     */
+    public String getB() {
+        return b;
+    }
+
+    /**
+     * @return the seme
+     */
+    public double getSeme() {
+        return seme;
+    }
+    
+    
+}
